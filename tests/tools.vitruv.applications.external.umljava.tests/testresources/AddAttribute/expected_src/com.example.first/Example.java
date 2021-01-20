30c00470309c1cf413c458a5a80ff8ed522061c6
Jan Wittler <dev.wittler@gmail.com>
2021-01-20 11:59:24 +0100

    corrected expected src files

--- tests/tools.vitruv.applications.external.umljava.tests/testresources/AddAttribute/expected_src/com.example.first/Example.java
+++ tests/tools.vitruv.applications.external.umljava.tests/testresources/AddAttribute/expected_src/com.example.first/Example.java
@@ -5,21 +5,21 @@ import java.lang.String;
 
 public class Example {
 	private String name;
-	private int counter;
 	public String getName() {
 		return this.name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	public boolean nameEquals(String otherName) {
+		return false;
+	}
+	private int counter;
 	public int getCounter() {
 		return this.counter;
 	}
 	public void setCounter(int counter) {
 		this.counter = counter;
-	}
-	public boolean nameEquals(String otherName) {
-		return false;
 	}
 }
 
--- tests/tools.vitruv.applications.external.umljava.tests/testresources/RemoveAttribute/expected_src/com.example.first/Example.java
+++ tests/tools.vitruv.applications.external.umljava.tests/testresources/RemoveAttribute/expected_src/com.example.first/Example.java
@@ -1,5 +1,7 @@
 package com.example.first ;
 
+import java.lang.String;
+
 
 public class Example {
 	public boolean nameEquals(String otherName) {
